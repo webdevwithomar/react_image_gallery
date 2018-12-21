5226a9620e974c55eb399306e54bfd90b30f5c1d
Omar Faruque <omar.faruque967@gmail.com>
2018-12-22 01:57:45 +0600

    fetch data from flickr

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+// All the imports
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 import Header from './components/Header'
 import PhotoContainer from './components/PhotoContainer';
+import apiKey from './config.js';
 
-class App extends Component {
+// Storing the apiKey into a variable
+const api = apiKey
+
+// The top level component
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      photos: []
+    }
+  }
+
+  // Lifecycle method
+  componentDidMount() {
+    axios.get(api)
+      .then((res) => {
+        this.setState({
+          photos: res.data
+        });
+      })
+      .catch(function (error) {
+        console.log('Error fetching data from flickr', error);
+      })
+  }
+
   render() {
+    console.log(this.state.photos);
     return (
       <div className="container">
         <Header />
@@ -13,5 +41,3 @@ class App extends Component {
     );
   }
 }
-
-export default App;
